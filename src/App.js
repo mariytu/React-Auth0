5c179023a5a040c7966d6ebe37fefa416b5ddcef
Marianela Iturriaga Jimenez <marianela.iturriaga@direcciondecompras.local>
2020-06-14 16:30:24 -0400

    Initial app structure.

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Route } from 'react-router-dom'
+import Home from './Home'
+import Profile from './Profile'
+import Nav from './Nav'
+
+const App = () => {
+  return (
+    <>
+      <Nav />
+      <div className="body">
+        <Route path="/" exact component={Home} />
+        <Route path="/profile" component={Profile} />
+      </div>
+    </>
+  )
+}
+
+export default App
